a64c67d6ba98aa5a37e81d7fe745244b399e5160
Papuc Vasile <32643188+CaReS0107@users.noreply.github.com>
2024-03-04 08:29:12 +0200

    Cleanup service provider

--- src/TranslatableServiceProvider.php
+++ src/TranslatableServiceProvider.php
@@ -10,16 +10,10 @@ class TranslatableServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('translatable')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_translatable_table')
             ->hasCommand(TranslatableCommand::class);
     }
 }
